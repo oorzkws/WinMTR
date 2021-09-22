5b22552ec58694ce783ba838f8c05b24813455f5
Vitaliy Zhukov <dragonzx@yandex.ru>
2021-09-22 12:35:08 +0300

    Release prepare

--- src/WinMTRDialog.cpp
+++ src/WinMTRDialog.cpp
@@ -115,9 +115,9 @@ BOOL WinMTRDialog::OnInitDialog()
 	}
 	
 #ifndef  _WIN64
-	char caption[] = {"WinMTR (Redux) v1.01 32bit"};
+	char caption[] = {"WinMTR (Redux) v1.02 32bit"};
 #else
-	char caption[] = {"WinMTR (Redux) v1.01 64bit"};
+	char caption[] = {"WinMTR (Redux) v1.02 64bit"};
 #endif
 	
 	SetTimer(1, WINMTR_DIALOG_TIMER, NULL);
